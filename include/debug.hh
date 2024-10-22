5f4d573eb989bd839bda619558db89f5fb57da9c
Fabrice <fabrice.schaub@swisscom.com>
2024-10-22 17:07:11 +0200

    windows support but unsure if it works

--- include/debug.hh
+++ include/debug.hh
@@ -2,16 +2,24 @@
 #define __UTILSCPP_DEBUG_HH__
 
 #define PRAGMA(desc) _Pragma(#desc)
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
 
 #ifdef DEBUG_MODE
 #define DEBUG(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
 #else
 #if defined(__GNUC__) || defined(__clang__)
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
 #define DEBUG(format, ...)                            \
   PRAGMA(GCC warning "Found DEBUG macro in " __FILE__ \
                      " at line " TOSTRING(__LINE__))
+#elif defined(_MSC_VER)
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#pragma warning(push)
+#define DEBUG(format, ...)                          \
+  __pragma(message("Found DEBUG macro in " __FILE__ \
+                   " at line " TOSTRING(__LINE__)))
+#pragma warning(pop)
 #else
 #define DEBUG(format, ...)
 #endif
