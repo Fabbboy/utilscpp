43dc08591f9020ad7fc363df26be6bdacc7d2434
Fabrice <fabrice.schaub@swisscom.com>
2024-10-22 16:54:28 +0200

    basic headers

---
+++ include/memory.hh
@@ -0,0 +1,27 @@
+#ifndef __UTILSCPP_MEMORY_HH__
+#define __UTILSCPP_MEMORY_HH__
+
+#include <memory>
+
+template <typename T>
+using Box = std::unique_ptr<T>;
+template <typename T>
+using Ref = std::shared_ptr<T>;
+template <typename T>
+using Raw = T*;
+
+template <typename T>
+constexpr Box<std::remove_reference_t<T>> make_box(T&& value) {
+  return std::make_unique<std::remove_reference_t<T>>(std::forward<T>(value));
+}
+template <typename T, typename... Args>
+constexpr Box<T> make_box(Args&&... args) {
+  return std::make_unique<T>(std::forward<Args>(args)...);
+}
+
+template <typename T, typename... Args>
+constexpr Ref<T> make_ref(Args&&... args) {
+  return std::make_shared<T>(std::forward<Args>(args)...);
+}
+
+#endif
---
+++ include/result.hh
@@ -0,0 +1,73 @@
+#ifndef __UTILSCPP_RESULT_HH__
+#define __UTILSCPP_RESULT_HH__
+
+#include <optional>
+#include <stdexcept>
+#include <variant>
+
+namespace utilscpp {
+struct ValueTag {};
+struct ErrorTag {};
+
+template <typename T, typename E>
+class Result {
+ private:
+  std::variant<T, E> value;
+  bool hasValue;
+
+ public:
+  Result(ValueTag, T v) : value(std::move(v)), hasValue(true) {}
+  Result(ErrorTag, E e) : value(std::move(e)), hasValue(false) {}
+
+  Result(const Result& other) = default;
+  Result(Result&& other) = default;
+  Result& operator=(const Result& other) = default;
+  Result& operator=(Result&& other) = default;
+
+  static Result Ok(T v) { return Result(ValueTag{}, std::move(v)); }
+  static Result Err(E e) { return Result(ErrorTag{}, std::move(e)); }
+
+  bool is_ok() const { return hasValue; }
+  bool is_err() const { return !hasValue; }
+
+  T unwrap() {
+    if (hasValue) return std::move(std::get<T>(value));
+    throw std::runtime_error("No value present, but error exists");
+  }
+
+  E unwrap_err() {
+    if (!hasValue) return std::move(std::get<E>(value));
+    throw std::runtime_error("No error present");
+  }
+};
+template <typename E>
+class Result<void, E> {
+ private:
+  std::optional<E> error;
+
+ public:
+  Result() : error(std::nullopt) {}
+  Result(ErrorTag, E e) : error(std::move(e)) {}
+
+  static Result Ok() { return Result(); }
+  static Result Err(E e) { return Result(ErrorTag{}, std::move(e)); }
+
+  bool is_ok() const { return !error.has_value(); }
+  bool is_err() const { return error.has_value(); }
+
+  void unwrap() {
+    if (is_err()) {
+      throw std::runtime_error("No value present, but error exists");
+    }
+  }
+
+  E unwrap_err() {
+    if (is_err()) {
+      return std::move(*error);
+    }
+    throw std::runtime_error("No error present");
+  }
+};
+}  // namespace utilscpp
+
+#endif  // __UTILSCPP_RESULT_HH__
