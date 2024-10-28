d62b3fcdc1a74a3d6ae93d2c0f647ba85d5e3ab4
Fabrice <schaubfabrice@gmail.com>
2024-10-28 23:13:22 +0100

    important macro

--- include/macro.hh
+++ include/macro.hh
@@ -5,5 +5,28 @@
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
 #define COMPTIME_ERR(msg) PRAGMA(GCC error msg)
+#define UNREACHABLE()                                                          \
+  THROW("Reached unreachable code in %s:%d", __FILE__, __LINE__)
+
+#ifdef __cplusplus
+#include <cstdio>
+#include <cstdlib>
+#define THROW(msg, ...)                                                        \
+  do {                                                                         \
+    int size = std::snprintf(nullptr, 0, msg, __VA_ARGS__) + 1;                \
+    std::unique_ptr<char[]> buffer(new char[size]);                            \
+    std::snprintf(buffer.get(), size, msg, __VA_ARGS__);                       \
+    throw std::runtime_error(buffer.get());                                    \
+  } while (0)
+#else
+#include <stdio.h>
+#include <stdlib.h>
+#define THROW(msg, ...)                                                        \
+  do {                                                                         \
+    fprintf(stderr, msg, __VA_ARGS__);                                         \
+    fprintf(stderr, "\n");                                                     \
+    abort();                                                                   \
+  } while (0)
+#endif
 
 #endif
